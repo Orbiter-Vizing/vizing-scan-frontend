8c67fc55b903614bf8dcbf7243f3225aa234a711
Mitten338 <mitten338@gmail.com>
2024-09-02 16:31:17 +0800

    feat: add repo style config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAppStyles } from "src/App.styles";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = (): JSX.Element => {
+  const classes = useAppStyles();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className={classes.appHeader}>
+        <h2>Welcome to Vizing Scan.</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <p className={classes.appIntro}>Vizing Scan is a comprehensive search, API, and analytics platform designed to streamline the experience for developers and users dealing with omnichain transactions.</p>
+    </div>
+  );
+};
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import "normalize.css/normalize.css";
+import { ThemeProvider } from "react-jss";
+
+import App from 'src/App'
+import { theme } from "src/styles/theme";
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
