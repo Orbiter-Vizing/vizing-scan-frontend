8d047b3beb9769bd5bde755d6b05ba9ef7d2c411
Mitten338 <mitten338@gmail.com>
2024-09-03 16:27:04 +0800

    feat: add header

---
+++ src/views/shared/header/header.styles.ts
@@ -0,0 +1,43 @@
+import { createUseStyles } from "react-jss";
+
+import { Theme } from "src/styles/theme";
+
+export const useHeaderStyles = createUseStyles((theme: Theme) => ({
+  headerWrap: {
+    position: "relative",
+    zIndex: 1,
+    display: "flex",
+    height: 64,
+  },
+  logoWrap: {
+    display: "flex",
+    alignItems: "center",
+    marginRight: 26,
+  },
+  headerTab: {
+    "&:hover": {
+      color: theme.palette.white.main,
+      cursor: "pointer",
+    },
+    display: "inline-flex",
+    height: "100%",
+    alignItems: "center",
+    padding: "0 12px 0",
+    color: theme.palette.white.transparency60,
+    marginRight: 12,
+  },
+  centerBlock: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: 400,
+  },
+  rightBlock: {
+    display: "flex",
+    alignItems: "center",
+  },
+  socialIcon: {
+    height: 16,
+    width: 16,
+    marginLeft: 20,
+  },
+}));
---
+++ src/views/shared/header/header.view.tsx
@@ -0,0 +1,71 @@
+import { FC } from "react";
+
+import { useHeaderStyles } from "src/views/shared/header/header.styles";
+import VizingScanLogo from "src/assets/vizing-scan-logo.svg?react";
+import VizingScanText from "src/assets/vizing-scan-text.svg?react";
+import twitterIcon from "src/assets/icon/social-media/twitter.svg?react";
+import discordIcon from "src/assets/icon/social-media/discord.svg?react";
+import mediumIcon from "src/assets/icon/social-media/medium.svg?react";
+import { routes } from "src/routes";
+
+const headerTabs = [
+  {
+    id: routes.messages.id,
+    text: routes.messages.text,
+    path: routes.messages.path,
+  },
+  {
+    id: routes.protocols.id,
+    text: routes.protocols.text,
+    path: routes.protocols.path,
+  },
+];
+
+const socialMediaList = [
+  {
+    id: "x-link",
+    link: "https://x.com/vizing_l2",
+    icon: twitterIcon,
+  },
+  {
+    id: "discord-link",
+    link: "https://discord.com/invite/FbztTBvnBT",
+    icon: discordIcon,
+  },
+  {
+    id: "medium-link",
+    link: "https://medium.com/@Vizing_L2",
+    icon: mediumIcon,
+  },
+];
+
+export const Header: FC = () => {
+  const classes = useHeaderStyles();
+
+  return (
+    <div className={classes.headerWrap}>
+      <div className={classes.logoWrap}>
+        <VizingScanLogo />
+        <VizingScanText />
+      </div>
+      <div className={classes.centerBlock}>
+        {headerTabs.map((tab) => {
+          return (
+            <span key={tab.id} className={classes.headerTab}>
+              {tab.text}
+            </span>
+          );
+        })}
+      </div>
+      <div className={classes.rightBlock}>
+        {socialMediaList.map((item) => (
+          <a key={item.id} href={item.link} target="_blank">
+            <span>
+              <item.icon className={classes.socialIcon} />
+            </span>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
